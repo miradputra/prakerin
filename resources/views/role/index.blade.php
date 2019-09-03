45dd45dec627255c72047a7250eb5d5f7dff3831
miradputra <miradputra@gmail.com>
2019-09-03 11:22:46 +0700

    senin

---
+++ resources/views/role/index.blade.php
@@ -0,0 +1,111 @@
+@extends('layout.master')
+
+@section('title')
+    <title>Manajemen Role</title>
+    @endsection
+
+    @section('content')
+        <div class="content-wrapper">
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0 text-dark">Manajemen Role</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"> <a href="{{route('home')}}">Home</a></li>
+                            <li class="breadcrumb-item active">Role</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-4">
+                    @card
+                                @slot('title')
+                                tambah
+                                @endslot
+
+                                @if (session('error'))
+                                    @alert(['type'=>'danger'])
+                                        {!!session('error')!!}
+                                    @endalert
+                                @endif
+
+                        <form role="form" action="{{ route('role.store')}}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <label for="name">Role</label>
+                            <input type="text" name="name" class="form-control{{ $error->has('name') ?'is-invalid':''}}" id="name" required>
+                            </div>
+                        @slot('footer')
+                            <div class="card-footer">
+                                <button class="btn btn-primary">Save</button>
+                            </div>
+                         </form>
+                         @endslot
+                    @endcard
+                        </div>
+                        <div class="col-md-8">
+                            @card
+                                @slot('title')
+                                List role
+                                @endslot
+
+                                @if(session('success'))
+                                    @alert(['type' => 'success'])
+                                        {!! session('success') !!}
+                                    @endalert
+                                @endif
+
+                                <div class="table-responsive">
+                                    <table class="table table-hover">
+                                        <thead>
+                                            <tr>
+                                                <td>#</td>
+                                                <td>Role</td>
+                                                <td>Guard</td>
+                                                <td>Created</td>
+                                                <td>Aksi</td>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @php $no = 1; @endphp
+                                            @forelse($role as $row)
+                                            <tr>
+                                                <td>{{$no++ }}</td>
+                                                <td>{{$row->name}}</td>
+                                                <td>{{$guard_name}}</td>
+                                                <td>{{$row->created_at}}</td>
+                                                <td>
+                                                    <form action="{{ route('role.destroy', $row->id) }}" method="POST">
+                                                        @csrf
+                                                        <input type="hidden" name="_method" value="DELETE">
+                                                        <button class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                            @empty
+                                            <tr>
+                                                <td colspan="5" class="text-center">Tidak ada data</td>
+                                            </tr>
+                                            @endforelse
+                                        </tbody>
+                                    </table>
+                                </div>
+
+                                <div class="float-right">
+                                    {!! $role->links() !!}
+                                </div>
+                                @endslot
+                                @endcard
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        @endsection
